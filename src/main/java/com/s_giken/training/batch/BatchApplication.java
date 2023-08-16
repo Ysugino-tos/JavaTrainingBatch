9f6767ab375b8b0621d02f0308b73c9312f7d34d
Ysugino-tos <sugino@tosplanning.co,jp>
2023-08-16 15:59:19 +0900

    初回コミット

---
+++ src/main/java/com/s_giken/training/batch/BatchApplication.java
@@ -0,0 +1,13 @@
+package com.s_giken.training.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchApplication.class, args);
+	}
+
+}
